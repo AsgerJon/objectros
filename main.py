9bce5069c3161bbf72f420db7438784cd8259dcb
Asger Jon Vistisen <asgerjon2@gmail.com>
2024-01-27 21:52:38 +0100

    cunts

--- main.py
+++ main.py
@@ -16,4 +16,3 @@ def tester00() -> None:
 
 if __name__ == '__main__':
   tester00()
-
---
+++ src/objectros/_ros_node.py
@@ -0,0 +1,11 @@
+"""RosNode encapsulates the nodes in the ros framework."""
+#  MIT Licence
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+
+
+class RosNode:
+  """RosNode encapsulates the nodes in the ros framework."""
+  
+  def __init__(self, *args, **kwargs) -> None:
+    pass
